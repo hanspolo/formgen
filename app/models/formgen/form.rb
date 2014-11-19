965725ed31831968ba77ede6f5f5544fe68679e0
Florian Thomas <flo@florianthomas.net>
2014-11-19 15:57:46 +0100

    A form requires a title

--- app/models/formgen/form.rb
+++ app/models/formgen/form.rb
@@ -4,6 +4,8 @@ module Formgen
     accepts_nested_attributes_for :questions, allow_destroy: true
 
     has_many :replies, dependent: :destroy
+    
+    validates :title, presence: true
 
     def to_csv
       CSV.generate do |csv|
